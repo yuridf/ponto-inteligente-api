1d9404e840ece81954652f1b1f1512a9563a6482
07319355 YURI BRAGA NEIVA <yuri.neiva@dccpm.local>
2018-09-27 16:20:55 -0300

    Arquivos iniciais do projeto

---
+++ src/main/java/br/com/pontointeligente/api/PontointeligenteapiApplication.java
@@ -0,0 +1,12 @@
+package br.com.pontointeligente.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PontointeligenteapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PontointeligenteapiApplication.class, args);
+    }
+}
